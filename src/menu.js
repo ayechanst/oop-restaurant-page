82733c4e80b094330d7f81f4fdae4f6cf192672b
Aye Chan <ayechan.sant@gmail.com>
2023-03-23 12:35:38 -0700

    added css

--- src/menu.js
+++ src/menu.js
@@ -2,9 +2,17 @@ import { content } from "./index.js";
 import { God } from "./index.js"
 
 export function menu() {
-    console.log('test from menu')
-    const menu = new God();
-    menu.makeElement('div', 'banner', 'this works')
-    content.appendChild(menu)
+    let menu = new God()
+    let title = new God()
+    let menuButton = new God()
+    let aboutButton = new God()
+    let homeButton = new God()
+    menu = menu.makeElement('div', 'banner', null,
+        title.makeElement('h1', 'title', 'The Feral Fox\'s Food'),
+        menuButton.makeElement('button', 'menu', 'menu'),
+        aboutButton.makeElement('button', 'about', 'About'),
+        homeButton.makeElement('button', 'home', 'Home')
+        )
 
+    content.appendChild(menu)
 }
