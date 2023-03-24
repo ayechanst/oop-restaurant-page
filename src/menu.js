eaadd9117c2954414e0d1ea20cbbf41a56b09b56
Aye Chan <ayechan.sant@gmail.com>
2023-03-24 07:03:54 -0700

    footer done

--- src/menu.js
+++ src/menu.js
@@ -1,18 +1,34 @@
 import { content } from "./index.js";
 import { God } from "./index.js"
 
-export function menu() {
-    let menu = new God()
+export function skeleton() {
+    let head = new God()
     let title = new God()
     let menuButton = new God()
     let aboutButton = new God()
     let homeButton = new God()
-    menu = menu.makeElement('div', 'banner', null,
+    head = head.makeElement('div', 'banner', null,
         title.makeElement('h1', 'title', 'The Feral Fox\'s Food'),
-        menuButton.makeElement('button', 'menu', 'menu'),
+        menuButton.makeElement('button', 'menu', 'Menu'),
         aboutButton.makeElement('button', 'about', 'About'),
         homeButton.makeElement('button', 'home', 'Home')
         )
+    content.appendChild(head)
 
-    content.appendChild(menu)
+    let body = new God()
+    body = body.makeElement('div', 'body', null)
+    content.appendChild(body)
+
+    let footer = new God()
+    let contactUs = new God()
+    let leaveAReview = new God()
+    let about = new God()
+    let blog = new God()
+    footer = footer.makeElement('div', 'footer', 
+        links.makeElement('div', 'footer-container', 
+        contactUs.makeElement('a', 'footer-link', 'Contact Us'),
+        leaveAReview.makeElement('a', 'footer-link', 'Leave A Review'),
+        about.makeElement('a', 'footer-link', 'About'),
+        blog.makeElement('a', 'footer-container', 'Blog')
+        ))
 }
